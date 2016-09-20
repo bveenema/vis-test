8cc72ab81d6e7acb540549dda2bd8e5763046e75
Veen <ben@veenemadesignworks.com>
2016-09-20 16:32:15 -0400

    Added Value Form Change

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ let yScale = d3.scaleLinear().range([height, 0]);
 
 
 // Create the SVG object with nested group
-let viz = d3.select("#viz-wrapper")
-                    .append('svg')
-                    .attr('height', height +2*padding)
-                    .attr('width', width + 2*padding)
-                    .append('g')
-                    .attr('id', 'viz')
-                    .attr('transform', 'translate(' + padding + ',' + padding + ')');
+let svg = d3.select("#viz-wrapper")
+              .append('svg')
+              .attr('height', height +2*padding)
+              .attr('width', width + 2*padding)
+
+let viz = svg.append('g')
+              .attr('id', 'viz')
+              .attr('transform', 'translate(' + padding + ',' + padding + ')');
 
 // Set up the x axis
 let xAxis = d3.axisBottom(xScale).ticks(5);
@@ -29,11 +30,14 @@ let trendline = d3.line()
   .y(function(d) { return yScale(d.value); })
   .curve(d3.curveMonotoneX);
 
+
 // Setup the drag behavior
 let dataDrag = d3.drag()
 								.on('start', dragStart)
 								.on('drag', dragging)
 								.on('end', dragEnd);
+
+let wasDraggedFLAG = false;
 
 // Add Data to the SVG
 d3.json('mock/data.json', function(error, data) {
@@ -74,7 +78,7 @@ d3.json('mock/data.json', function(error, data) {
               .data(data)
               .enter()
               .append('g')
-              .attr('class', 'data-point');
+              .attr('class', 'data-point')
   
   dataPoints.attr('transform', function(d){
 	  	x = xScale(d3.isoParse(d.date));
@@ -85,38 +89,82 @@ d3.json('mock/data.json', function(error, data) {
 		.style('fill', 'blue');
 	
 	dataPoints.append('circle').attr('r', defaultRadius);
-	
-	dataPoints.append('text')
-		.text(function(d){return d.value})
-		.attr('transform','translate(5,5)')
-		.style('fill', 'green')
-		.style('display', 'none');
+
+	let tooltip = dataPoints.append('g')
+													.attr('class', 'tooltip hide')
+													.attr('transform','translate(5,5)');
+	tooltip.append('text')
+					.text(function(d){return d.value});
 
 // Display text on hover
 	dataPoints.on("mouseenter", function(d, i) {
     d3.select(this)
-    	.select('text')
-      .style('display', 'block');
+    	.select('g.tooltip')
+      .classed('show', true)
+      .classed('hide', false);
   });
 
   dataPoints.on("mouseleave", function(d, i) {
     d3.select(this)
-      .select('text')
-      .style('display', 'none');
+      .select('g.tooltip')
+      .classed('show', false)
+      .classed('hide', true);
   });
 
-// Add click event handler
+// Add click event handlers
+	svg.on("click", function(){
+		console.log('viz clicked');
+		d3.selectAll('g.data-point')
+			.select('circle')
+			.classed('selected', false);
+    d3.select('#updator').attr('class', 'hide');
+	});
 	dataPoints.on("click", function(d, i){
 		if (d3.event.defaultPrevented) return;
-		console.log("clicked");
+		d3.event.stopPropagation();
+		d3.selectAll('g.data-point')
+			.select('circle')
+			.classed('selected', false);
 		d3.select(this)
 			.select('circle')
-			.style('fill', 'red');
+			.classed('selected', true);
+		d3.select("#updator")
+			.attr('class', 'show')
+			.attr('dataPoint', i);
+		d3.select("#value")
+			.attr('placeholder', d.value);
 	});
 
 // Attach the drag behavior to the data points
 	dataPoints.call(dataDrag);
 
+// Add #updator submit handler
+	d3.select('#updator')
+		.on('submit', function(){
+			d3.event.preventDefault();
+
+			console.log();
+
+			//get the new value
+			let newValue = parseInt(document.getElementById('value').value)
+			
+			//select the corresponding data point and upate it's value and location
+			let i = parseInt(d3.select(this).attr('dataPoint'));
+			data[i].value = newValue;
+			let newPositionY = yScale(data[i].value);
+			d3.select(dataPoints._groups[0][i])
+				.attr("transform", 
+      				'translate(436,' +
+                     newPositionY + ')');
+
+			//recalculate trendline
+		 	d3.select('.trendline')
+		 		.attr('d', trendline);
+
+			//hide the updator
+			d3.select(this)
+				.attr('class', 'hide');
+		});
 });
 
 
@@ -125,7 +173,6 @@ d3.json('mock/data.json', function(error, data) {
 function dragStart(d) {
   d3.event.sourceEvent.stopPropagation();
   d3.event.sourceEvent.preventDefault();
-  console.log('dragStart');
   d3.select(this)
      .select('circle')
      .attr('r', defaultRadius*1.15);
@@ -137,7 +184,7 @@ function dragStart(d) {
 //the new data value and date, updating the text in the group,
 //and recalculating the fit line
 function dragging(d) {
-	console.log('dragging');
+	wasDraggedFLAG = true;
   d.x = d3.event.x;
   d.y = d3.event.y;
   d3.select(this)
@@ -160,11 +207,16 @@ function dragging(d) {
 //Takes the new data point and sends it to the server(TODO) or console(CURRENT)
 //Reduces the circle radius back to its original size
 function dragEnd(d){
-	console.log('dragEnd');
 	d3.select(this)
 		.select('circle')
 		.attr('r',defaultRadius);
- console.log(d);
+	
+	// Send new data if dataPoint wasDragged
+	if(wasDraggedFLAG){
+		console.log(d);
+		wasDraggedFLAG = false;
+	}
+ 	
 }
 
 
